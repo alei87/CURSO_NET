fd704511788f8a6d1cd33a85e902f25246e63f05
Aleida Rodriguez Garcia <aleida1987rg@gmail.com>
2021-07-16 16:23:48 +0200

    Insertado enunciado del ejercicio

--- CONSOLA_BANCO/CONSOLA_BANCO/Program.cs
+++ CONSOLA_BANCO/CONSOLA_BANCO/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace CONSOLA_BANCO
 {
+    //Ejercicio para gestionar las cuentas de un banco
+    //Se pide crear una clase tipo Cuenta para gestionar ingresos y gastos
     class Program
     {
         static void Main(string[] args)
