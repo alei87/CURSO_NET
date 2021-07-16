3c7525515e4413c9dabf1c2a01cdaddf96a1b69d
Aleida Rodriguez Garcia <aleida1987rg@gmail.com>
2021-07-16 19:09:45 +0200

    crear case animal

---
+++ CONSOLA_ZOO/CONSOLA_ZOO/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSOLA_ZOO
+{
+   abstract class Animal
+    {
+        protected string especie;
+        protected string nombre;
+        protected double peso;
+        protected int numJaula;
+
+    }
+}
