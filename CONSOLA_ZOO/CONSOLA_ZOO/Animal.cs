88397f15b6fb256a553b4601336e7e3f59d55b4e
Aleida Rodriguez Garcia <aleida1987rg@gmail.com>
2021-07-16 20:00:15 +0200

    subida clase animal y clase mamifero

--- CONSOLA_ZOO/CONSOLA_ZOO/Animal.cs
+++ CONSOLA_ZOO/CONSOLA_ZOO/Animal.cs
@@ -6,12 +6,49 @@ using System.Threading.Tasks;
 
 namespace CONSOLA_ZOO
 {
-   abstract class Animal
+    abstract class Animal
     {
         protected string especie;
         protected string nombre;
         protected double peso;
         protected int numJaula;
 
+        public string Especie
+        {
+            get { return especie; }
+            set { especie = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+        
+        public int NumJaula
+        {
+            get { return numJaula; }
+            set { numJaula = value; }
+        }
+
+        public Animal() { }
+        public Animal(string p_especie, string p_nombre, double p_peso, int p_numJaula)
+        {
+            p_especie = especie;
+            p_nombre = nombre;
+            p_peso = peso;
+            p_numJaula = numJaula;
+        }
+
+        public virtual void ClaseDeAnimalEres()
+        {
+            
+        }
     }
 }
---
+++ CONSOLA_ZOO/CONSOLA_ZOO/Mamifero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSOLA_ZOO
+{
+      class Mamifero: Animal
+    {
+        public Mamifero() { }
+
+        public Mamifero(string especie,string nombre, double peso, int numJaula ): base(especie,nombre, peso, numJaula) { }
+
+        public override void ClaseDeAnimalEres()
+        {
+            Console.WriteLine("Soy un " + this.Especie);
+            Console.WriteLine("Mi nombre es " + this.Nombre);
+            Console.WriteLine("Mi peso es " + this.Peso);
+            Console.WriteLine("Mi número de jaula es " + this.NumJaula);
+        }
+    }
+}
