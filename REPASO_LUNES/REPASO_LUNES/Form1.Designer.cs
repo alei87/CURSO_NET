cbb2ec679365c36875670765616f9995dd87f2a8
Aleida Rodriguez Garcia <aleida1987rg@gmail.com>
2021-07-23 16:08:55 +0200

    SUBIENDO DISEÑO FORMULARIO

---
+++ REPASO_LUNES/REPASO_LUNES/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace REPASO_LUNES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListID = new System.Windows.Forms.ListView();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.DataGridDatos = new System.Windows.Forms.DataGridView();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.BtnModificar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtIDcliente = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtApellidos = new System.Windows.Forms.TextBox();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListID
+            // 
+            this.ListID.HideSelection = false;
+            this.ListID.Location = new System.Drawing.Point(24, 22);
+            this.ListID.Name = "ListID";
+            this.ListID.Size = new System.Drawing.Size(121, 22);
+            this.ListID.TabIndex = 0;
+            this.ListID.UseCompatibleStateImageBehavior = false;
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(169, 21);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(322, 23);
+            this.BtnBuscar.TabIndex = 1;
+            this.BtnBuscar.Text = "BUSCAR";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // DataGridDatos
+            // 
+            this.DataGridDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridDatos.Location = new System.Drawing.Point(24, 68);
+            this.DataGridDatos.Name = "DataGridDatos";
+            this.DataGridDatos.Size = new System.Drawing.Size(467, 291);
+            this.DataGridDatos.TabIndex = 2;
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Location = new System.Drawing.Point(80, 385);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(110, 34);
+            this.BtnAgregar.TabIndex = 3;
+            this.BtnAgregar.Text = "AGREGAR";
+            this.BtnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(226, 385);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(115, 34);
+            this.BtnEliminar.TabIndex = 4;
+            this.BtnEliminar.Text = "ELIMINAR";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // BtnModificar
+            // 
+            this.BtnModificar.Location = new System.Drawing.Point(370, 385);
+            this.BtnModificar.Name = "BtnModificar";
+            this.BtnModificar.Size = new System.Drawing.Size(121, 34);
+            this.BtnModificar.TabIndex = 5;
+            this.BtnModificar.Text = "MODIFICAR";
+            this.BtnModificar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(535, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "APELLIDOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(535, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "IDCLIENTE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(535, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "NOMBRE";
+            // 
+            // TxtIDcliente
+            // 
+            this.TxtIDcliente.Location = new System.Drawing.Point(604, 82);
+            this.TxtIDcliente.Name = "TxtIDcliente";
+            this.TxtIDcliente.Size = new System.Drawing.Size(154, 20);
+            this.TxtIDcliente.TabIndex = 9;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(604, 144);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(154, 20);
+            this.TxtNombre.TabIndex = 10;
+            // 
+            // TxtApellidos
+            // 
+            this.TxtApellidos.Location = new System.Drawing.Point(604, 201);
+            this.TxtApellidos.Name = "TxtApellidos";
+            this.TxtApellidos.Size = new System.Drawing.Size(154, 20);
+            this.TxtApellidos.TabIndex = 11;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Location = new System.Drawing.Point(637, 279);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(121, 34);
+            this.BtnSalir.TabIndex = 12;
+            this.BtnSalir.Text = "SALIR";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.TxtApellidos);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.TxtIDcliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnModificar);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.BtnAgregar);
+            this.Controls.Add(this.DataGridDatos);
+            this.Controls.Add(this.BtnBuscar);
+            this.Controls.Add(this.ListID);
+            this.Name = "Form1";
+            this.Text = "EJER_REPASO";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView ListID;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.DataGridView DataGridDatos;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Button BtnModificar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtIDcliente;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox TxtApellidos;
+        private System.Windows.Forms.Button BtnSalir;
+    }
+}
+
