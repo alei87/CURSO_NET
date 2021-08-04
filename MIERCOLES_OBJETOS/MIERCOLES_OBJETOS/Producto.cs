a6358dc204310b69122e7a80dde809ffa9d94de0
Aleida Rodriguez Garcia <aleida1987rg@gmail.com>
2021-08-04 20:26:32 +0200

    subida clase y empezar el pedido

---
+++ MIERCOLES_OBJETOS/MIERCOLES_OBJETOS/Producto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIERCOLES_OBJETOS
+{
+     public class Producto
+    {
+       private string nombre;
+       private double precio;
+       private string categoria;
+
+        public Producto()
+        {
+
+        }
+        public Producto(string nombre, double precio, string categoria)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.categoria = categoria;
+
+        }
+        public Producto(string nombre, double precio)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public double Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+
+        public string Categoria
+        {
+            get { return categoria; }
+            set { categoria = value; }
+        }
+        
+        public void Pedir()
+        {
+            Console.WriteLine("")
+           for(int cant) 
+        }
+        
+
+    }
+}
---
+++ MIERCOLES_OBJETOS/MIERCOLES_OBJETOS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIERCOLES_OBJETOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto PR1 = new Producto();
+            
+
+            string nom = "";
+            double pre = 0;
+            string cat = "";
+            string opcion = "";
+
+            do
+            {
+                Console.WriteLine("1-Crear el Producto");
+                Console.WriteLine("2-Haz un Pedido");
+                Console.WriteLine("3Muestra el Total + IVA");
+                Console.WriteLine("4-Salir");
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Introduce el nombre");
+                        PR1.Nombre = Console.ReadLine();
+                        Console.WriteLine("Introduce el precio");
+                        PR1.Precio = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduce la categoria");
+                        PR1.Categoria = Console.ReadLine();
+                        break;
+
+                    case "2":
+
+                }
+            }
+
+            
+        }
+    }
+}
