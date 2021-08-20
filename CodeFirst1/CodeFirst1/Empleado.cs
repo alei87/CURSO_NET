4e677055421bf99d687403e895a2987a2bc04e5c
Aleida Rodriguez Garcia <aleida1987rg@gmail.com>
2021-08-20 20:17:03 +0200

    primera subida de ejercicio 1 de codeFirst

---
+++ CodeFirst1/CodeFirst1/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst1
+{
+    public class Empleado
+    {
+        [Key]
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+
+        public string Email { get; set; }
+    }
+}
---
+++ CodeFirst1/CodeFirst1/Models/ContextoEmpleados.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst1.Models
+{
+    public class ContextoEmpleados:DbContext
+    {
+        public ContextoEmpleados() : base("CadenaEmpleados") { }
+        public DbSet<Empleado> Empleados { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        
+    }
+}
