fe347d31b5f903111aacfdde571fcadbf7a0e7fb
Aleida Rodriguez Garcia <aleida1987rg@gmail.com>
2021-09-06 16:09:13 +0200

    subida primer ejemplo MVC

---
+++ MVC_Viernes/MVC_Viernes/Models/Biblioteca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Viernes.Models
+{
+    public class Biblioteca
+    {
+        public List<Libro> Libros { get; set; }
+        public Biblioteca()
+        {
+            Libros = new List<Libro>
+            {
+                new Libro{ISBN="11122", Titulo="Los Piratas del Caribe", TipoLibro="Novela"},
+                new Libro{ISBN="22211", Titulo="Los Pilares de la Tierra", TipoLibro="Novela"},
+                new Libro {ISBN = "12121", Titulo = "Steve Jobs", TipoLibro = "Biografía" }
+            };
+        }
+
+    }
+}
